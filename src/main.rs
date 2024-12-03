7d7ac8a7a4751cb293bd6d765ceca2a9261466f2
Aly <aly.abdelmoneim@student.giu-uni.de>
2024-12-03 03:38:11 +0200

    shitty not working version, never come back here

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,51 @@
-use clap::{Arg, Command};
-use crate::cli::{add_task, remove_task, list_tasks};
+mod cli;
+mod tasks;
+
+use clap::{Arg, Command, ArgMatches, command};
 use crate::tasks::TaskPriority;
 
-
 fn main() {
-    let matches = Command::new("Todo CLI")
-        .version("1.0")
-        .author("Your Name <aliabubeih@gmail.com>")
-        .about("Manage your tasks")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("add")
-                .about("Add a new task")
-                .arg(Arg::new("title").about("Title of the task").required(true))
-                .arg(Arg::new("description").about("Description of the task").required(true))
-                .arg(Arg::new("priority").about("Priority of the task").required(true))
-        )
-        .subcommand(
-            Command::new("remove")
-                .about("Remove a task")
-                .arg(Arg::new("id").about("ID of the task").required(true))
-        )
-        .subcommand(
-            Command::new("list")
-                .about("List all tasks")
-        )
-        .get_matches();
+    let match_result = command!()
+    .about("Testing ")
+    .subcommand(
+        Command::new("add")
+        .about("Add a new task")
+                .arg(Arg::new("title"))
+                .arg(Arg::new("description"))
+                .arg(Arg::new("priority"))
+    )
+    .subcommand(
+        Command::new("remove")
+        .about("Remove a task")
+                .arg(Arg::new("id"))
+                .arg(Arg::new("title"))
+    )
+    .subcommand(
+        Command::new("list")
+        .about("List all tasks")
+                .arg(Arg::new("title"))
+                .arg(Arg::new("priority"))
 
-    match matches.subcommand() {
-        Some(("add", args)) => {
-            let title = args.value_of("title").unwrap();
-            let description = args.value_of("description").unwrap();
-            let priority = match args.value_of("priority").unwrap() {
-                "low" => TaskPriority::Low,
-                "medium" => TaskPriority::Medium,
-                "high" => TaskPriority::High,
-                _ => TaskPriority::Low,
-            };
-            add_task(title.to_string(), description.to_string(), priority);
-        }
-        Some(("remove", args)) => {
-            let id = args.value_of("id").unwrap().parse().unwrap();
-            remove_task(id);
-        }
-        Some(("list", _)) => {
-            list_tasks();
-        }
-        _ => unreachable!(),
+    )
+    .get_matches();
+
+    match match_result.subcommand() {
+        Some(("add", match_result)) => add_task_2(match_result),
+        Some(("remove", match_result)) => remove_task_2(match_result),
+        Some(("list", match_result)) => list_tasks_2(match_result),
+        _ => println!("No command provided"),
     }
+}
+fn add_task_2(match_result : ArgMatches) {
+    let title = match_result.get_one::<String>("title").unwrap().to_string();
+    let description = match_result.get_one::<String>("description").unwrap().to_string();
+    let priority = match_result.get_one::<TaskPriority>("priority").unwrap();
+    cli::add_task(title, description, priority);
+}
+fn remove_task_2(match_result : ArgMatches) {
+    let id = match_result.get_one::<u64>("id").unwrap();
+    cli::remove_task(*id);
+}
+fn list_tasks_2(match_result : ArgMatches) {
+    cli::list_tasks();
 }
